69ada950a52fc96ff3b87212e3741f5bd51afb90
atakankar <50875567+atakankar@users.noreply.github.com>
2020-02-02 16:18:44 +0300

    Add files via upload

---
+++ XOX/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace XOX
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.TabStop = false;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.XOXtiklama);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.TabStop = false;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.XOXtiklama);
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.TabStop = false;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.XOXtiklama);
+            // 
+            // button4
+            // 
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.TabStop = false;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.XOXtiklama);
+            // 
+            // button5
+            // 
+            resources.ApplyResources(this.button5, "button5");
+            this.button5.Name = "button5";
+            this.button5.TabStop = false;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.XOXtiklama);
+            // 
+            // button6
+            // 
+            resources.ApplyResources(this.button6, "button6");
+            this.button6.Name = "button6";
+            this.button6.TabStop = false;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.XOXtiklama);
+            // 
+            // button7
+            // 
+            resources.ApplyResources(this.button7, "button7");
+            this.button7.Name = "button7";
+            this.button7.TabStop = false;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.XOXtiklama);
+            // 
+            // button8
+            // 
+            resources.ApplyResources(this.button8, "button8");
+            this.button8.Name = "button8";
+            this.button8.TabStop = false;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.XOXtiklama);
+            // 
+            // button9
+            // 
+            resources.ApplyResources(this.button9, "button9");
+            this.button9.Name = "button9";
+            this.button9.TabStop = false;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.XOXtiklama);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Name = "label1";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ XOX/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XOX
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void XOXtiklama(object sender, EventArgs e)
+        {
+            Button bt = sender as Button;
+            if(label1.Text=="X")
+            {
+                bt.Text = "X";
+                label1.ForeColor = Color.Blue;
+                bt.Enabled = false;
+                label1.Text = "O";
+            }
+            else
+            {
+                bt.Text = "O";
+                label1.ForeColor = Color.Red;
+                bt.Enabled = false;
+                label1.Text = "X";
+            }
+
+            //KAZANMA DURUMLARI
+            if(button1.Text==button2.Text && button2.Text== button3.Text && button1.Enabled== false && button2.Enabled==false && button3.Enabled==false)
+            {
+                string a = button1.Text.ToString();
+                MessageBox.Show(a + " KAZANDI");
+                oyunbitis();
+            }
+            if (button4.Text == button5.Text && button5.Text == button6.Text && button4.Enabled == false && button5.Enabled == false && button6.Enabled == false)
+            {
+                string a = button4.Text.ToString();
+                MessageBox.Show(a + " KAZANDI");
+                oyunbitis();
+            }
+            if (button7.Text == button8.Text && button8.Text == button9.Text && button7.Enabled == false && button8.Enabled == false && button9.Enabled == false)
+            {
+                string a = button7.Text.ToString();
+                MessageBox.Show(a + " KAZANDI");
+                oyunbitis();
+            }
+            if (button1.Text == button4.Text && button4.Text == button7.Text && button1.Enabled == false && button4.Enabled == false && button7.Enabled == false)
+            {
+                string a = button1.Text.ToString();
+                MessageBox.Show(a + " KAZANDI");
+                oyunbitis();
+            }
+            if (button2.Text == button5.Text && button5.Text == button8.Text && button2.Enabled == false && button5.Enabled == false && button8.Enabled == false)
+            {
+                string a = button2.Text.ToString();
+                MessageBox.Show(a + " KAZANDI");
+                oyunbitis();
+            }
+            if (button3.Text == button6.Text && button6.Text == button9.Text && button3.Enabled == false && button6.Enabled == false && button9.Enabled == false)
+            {
+                string a = button3.Text.ToString();
+                MessageBox.Show(a + " KAZANDI");
+                oyunbitis();
+            }
+            if (button1.Text == button5.Text && button5.Text == button9.Text && button1.Enabled == false && button5.Enabled == false && button9.Enabled == false)
+            {
+                string a = button1.Text.ToString();
+                MessageBox.Show(a + " KAZANDI");
+                oyunbitis();
+            }
+            if (button3.Text == button5.Text && button5.Text == button7.Text && button3.Enabled == false && button5.Enabled == false && button7.Enabled == false)
+            {
+                string a = button3.Text.ToString();
+                MessageBox.Show(a + " KAZANDI");
+                oyunbitis();
+            }
+            //BERABERLİK
+            if(button1.Enabled==false && button2.Enabled == false && button3.Enabled == false && button4.Enabled == false && button5.Enabled == false && button6.Enabled == false && button7.Enabled == false && button8.Enabled == false && button9.Enabled == false )
+            {
+                MessageBox.Show("BERABERE");
+                oyunbitis();
+            }
+        }
+        //oyun bitişi     
+        private void oyunbitis()
+        {
+            label1.ForeColor = Color.Red;
+            label1.Text = "X";
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+            button5.Text = "";
+            button6.Text = "";
+            button7.Text = "";
+            button8.Text = "";
+            button9.Text = "";
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button6.Enabled = true;
+            button7.Enabled = true;
+            button8.Enabled = true;
+            button9.Enabled = true;
+        }
+    }
+}
